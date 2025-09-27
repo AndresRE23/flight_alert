b7861c1ecfb7ac690c79da6a920efd74d2b00007
Andres Rivera Espinoza <andresre@Andress-MacBook-Pro.local>
2025-09-26 19:47:00 -0600

    feat: get sheety data

--- data_manager.py
+++ data_manager.py
@@ -1,3 +1,16 @@
+import requests
+import os
+from dotenv import load_dotenv
+
 class DataManager:
     #This class is responsible for talking to the Google Sheet.
-    pass
+    def __init__(self):
+        load_dotenv()
+        self.sheety_endpoint = os.getenv("SHEETY_ENDPOINT")
+        self.sheet_data = {}
+
+    def get_destination_data(self):
+        response = requests.get(self.sheety_endpoint)
+        data = response.json()
+        self.sheet_data = data["prices"]
+        return self.sheet_data
