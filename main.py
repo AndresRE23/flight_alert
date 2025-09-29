034cec1602a8795145a9a23a11a75c275a20a4ae
Andres Rivera Espinoza <andresre@Andress-MacBook-Pro.local>
2025-09-28 20:27:29 -0600

    feat: handle empty IATA codes with placeholder 'TESTING'

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from data_manager import DataManager
+from flight_search import FlightSearch
 from pprint import pprint
 
 data_manager = DataManager()
+flight_search = FlightSearch()
 sheet_data = data_manager.get_destination_data()
+
+for flight in sheet_data:
+    if flight['iataCode'] == "":
+        code = flight_search.get_destination_code(flight['city'])
+        flight["iataCode"] = code
+
 pprint(sheet_data)
-
-
 
 
