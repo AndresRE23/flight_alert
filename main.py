b726932b3c95ebb24ab2f5fd6040b44d67720e93
AndresRE23 <andresre@icloud.com>
2025-09-30 17:16:45 -0600

    style: remove unused import and reformat code for readability

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 from data_manager import DataManager
 from flight_search import FlightSearch
 from pprint import pprint
-import time
 
 data_manager = DataManager()
 flight_search = FlightSearch()
-data_manager.get_destination_data()
 
+data_manager.get_destination_data()
 data_manager.update_destination_codes()
 
 updated_sheet_data = data_manager.get_destination_data()
